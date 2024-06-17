92ea2845ae9e56014134b95ef02fea25d33f0644
CASician <cristian.sician@gmail.com>
2024-06-17 16:57:30 +0200

    feat: add logic to handle the data structures

--- ppm_products/admin.py
+++ ppm_products/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
-from .models import my_Product
 
-admin.site.register(my_Product)
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
